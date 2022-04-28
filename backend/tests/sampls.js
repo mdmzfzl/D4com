277b52d135687b2ebf0e84589983692428959c14
unknown <logu.r19@iiits.in>
2022-04-28 14:04:03 +0530

    Update workflow

--- backend/tests/sampls.js
+++ backend/tests/sampls.js
@@ -1,26 +1,26 @@
 // import request from "supertest";
 // import app from "../server";
 
-async function main() {
-  /* using 20 to make the progress bar length 20 charactes, multiplying by 5 below to arrive to 100 */
+// async function main() {
+//   /* using 20 to make the progress bar length 20 charactes, multiplying by 5 below to arrive to 100 */
 
-  for (let i = 0; i <= 20; i++) {
-    const dots = ".".repeat(i);
-    const left = 20 - i;
-    const empty = " ".repeat(left);
+//   for (let i = 0; i <= 20; i++) {
+//     const dots = ".".repeat(i);
+//     const left = 20 - i;
+//     const empty = " ".repeat(left);
 
-    /* need to use  `process.stdout.write` becuase console.log print a newline character */
-    /* \r clear the current line and then print the other characters making it looks like it refresh*/
-    process.stdout.write(`\r[${dots}${empty}] ${i * 5}%`);
-    await wait(80);
-  }
-}
+//     /* need to use  `process.stdout.write` becuase console.log print a newline character */
+//     /* \r clear the current line and then print the other characters making it looks like it refresh*/
+//     process.stdout.write(`\r[${dots}${empty}] ${i * 5}%`);
+//     await wait(80);
+//   }
+// }
 
-// main();
+// // main();
 
-function wait(ms) {
-  return new Promise((res) => setTimeout(res, ms));
-}
+// function wait(ms) {
+//   return new Promise((res) => setTimeout(res, ms));
+// }
 
 // describe("Authentication User", function () {
 //   it("should login user", function (done) {
